720de16007f4298785d3e4eaeb7e3214aaba0455
Martin Jean-Christio <admin@marjc.ch>
2021-11-08 18:08:48 +0100

    Update

--- ft_printf/ft_printf_char.c
+++ ft_printf/ft_printf_char.c
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 12:37:37 by jmartin           #+#    #+#             */
-/*   Updated: 2021/11/08 17:26:28 by jmartin          ###   ########.fr       */
+/*   Updated: 2021/11/08 18:08:36 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,7 @@
 
 int	ft_printf_char(int c, int *rcount)
 {
-	if (c == 0)
-		ft_putchar_fd((unsigned char) '\0', 1);
-	else if (ft_isascii(c))
-		ft_putchar_fd((unsigned char)c, 1);
-	else if (!ft_isprint(c))
-		ft_putchar_fd((unsigned char) '0', 1);
+	ft_putchar_fd(c, 1);
 	*rcount += 1;
 	return (*rcount);
 }
